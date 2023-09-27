d19b52455e5867290554318f5bf8422275521a94
Gesspp <nikitatum199@gmail.com>
2023-09-27 13:25:28 +0300

    оформлен вход и регистрация

---
+++ InTouch/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+	import Signup from './components/Signup.vue'
+	import Signin from './components/Signin.vue'
+	export default{
+		components: {
+			Signup,
+			Signin
+		}
+	}
+</script>
+<template>
+	<Signin></Signin>
+</template>
+<style>
+
+</style>
---
+++ InTouch/src/components/Signin.vue
@@ -0,0 +1,39 @@
+<template>
+    <main>
+        <input type="text" class="nickname" placeholder="nickname">
+        <input type="text" class="password" placeholder="password">
+        <button>Sign up</button>
+    </main>
+</template>
+<script>
+
+</script>
+<style>
+    body{
+        margin: 0;
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    main{
+        width: 250px;
+        height: 350px;
+        gap: 20px;
+        background: #1889ab;
+        border-radius: 10px;
+        box-shadow: 4px 4px 3px 1px rgba(0, 0, 0, 0.2);
+        display: flex;
+        padding: 0;
+    }
+
+    .nickname, .password{ 
+        width: 200px;
+        height: 30px;
+        border-radius: 8px;
+        font-size: 15px;
+
+    }
+</style>
